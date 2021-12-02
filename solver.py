a77d58f9f2b37b2737963a204091be352142a8dc
jaysonyw <54644583+jaysonyw@users.noreply.github.com>
2021-12-01 18:53:11 -0800

    bug fixes and stuff

--- solver.py
+++ solver.py
@@ -2,33 +2,28 @@ from parse import read_input_file, write_output_file
 import os
 import Task
 
-def find_bucket_schedule(tasks, schedule, start_time, total_time=0):
+def find_schedule(tasks, start_time=0, buckets=1):
+
+    def find_schedule_helper(tasks, schedule=[], elapsed_time=0):
         if not tasks:
-            return schedule, calc_benefit(schedule, start_time, start_time + 288), start_time + total_time
+            return schedule, calc_benefit(schedule, start_time, start_time + (1440/buckets)), elapsed_time
 
         task = tasks[0]
 
-        return max(find_bucket_schedule(tasks[1:], schedule + [task], start_time, total_time + task.get_duration()), 
-                   find_bucket_schedule(tasks[1:], schedule, start_time, total_time), 
+        return max(find_schedule_helper(tasks[1:], schedule + [task], elapsed_time + task.get_duration()), 
+                   find_schedule_helper(tasks[1:], schedule, elapsed_time), 
                    key = lambda k: k[1])
 
-def find_schedule(tasks, schedule=[]):
-    if not tasks:
-        return schedule, calc_benefit(schedule, 0, 1440)
+    return find_schedule_helper(tasks)
 
-    return max(find_schedule(tasks[1:], schedule + [tasks[0]]), 
-               find_schedule(tasks[1:], schedule), 
-               key = lambda k: k[1])
-
-def calc_benefit(tasks, start_time, max_time=1440):
+def calc_benefit(tasks, start_time=0, max_time=1440):
     benefit = 0
-    time = start_time
     for task in tasks: 
-        if time + task.get_duration() < max_time:
+        if start_time + task.get_duration() < max_time:
             latest_start = task.get_deadline() - task.get_duration()
-            mins_late = max(0, time - latest_start)
+            mins_late = max(0, start_time - latest_start)
             benefit += task.get_late_benefit(mins_late)
-            time = time + task.get_duration()
+            start_time = start_time + task.get_duration()
         else:
             return 0
 
@@ -45,17 +40,8 @@ def zero_calibrate(tasks):
             task.deadline = task.get_duration()
     return tasks
 
-def remove_weak(tasks):
-    benefit = 0
-    for task in tasks:
-        benefit += task.get_max_benefit()
-
-    average_benefit = benefit/len(tasks)
-
-    return [task for task in tasks if task.get_max_benefit() > average_benefit * .1]
-
 def calc_task_heuristic(task):
-    return (task.get_deadline() - task.get_duration()) + 0 * task.get_max_benefit()
+    return (task.get_deadline() - task.get_duration()) - .5 * task.get_max_benefit()
 
 def solve(tasks):
     """
@@ -64,46 +50,45 @@ def solve(tasks):
     Returns:
         output: list of igloos in order of polishing  
     """
-
-    tasks = sorted(tasks, key=lambda x: calc_task_heuristic(x))
+    tasks = sorted(tasks, key=lambda x: x.get_deadline() - x.get_duration())
     tasks = zero_calibrate(tasks)
-    #tasks = remove_weak(tasks)
+    stack = []
+    big_task_dict = {}
+    num_buckets = 5
+    end_time = 0
 
-    big_schedule = []
-    big_benefit = 0
-    leftovers = []
-    start_time = 0
+    for i in range(num_buckets):
+        crabs = sorted(tasks[20*i:20*(i+1)], key=lambda x: calc_task_heuristic(x))
+        bucket = crabs[:18]
+        buffer = crabs[18:]
 
-    for i in range(5):
-
-        bucket = [tasks.pop(0) for i in range(19)]
-        buffer = [tasks.pop(0) for i in range(1)]
-
-        schedule, benefit, start_time = find_best_schedule(bucket, [], start_time)
-        big_schedule += schedule
-        big_benefit += benefit
+        schedule, benefit, end_time = find_schedule(bucket, end_time, num_buckets)
         duration = sum([task.get_duration() for task in schedule])
 
-        leftovers.append(Task.Task(100 + i, start_time, int(duration), float(benefit)))
-        leftovers += buffer
+        big_task = Task.Task(100+i+1, (i+1) * 288, int(duration), float(benefit))
+        big_task_dict[big_task] = schedule
 
-        ""
-        for task in schedule:
-            print(task)
-        ""
-        print("dur:", duration)
-        print("ben:", benefit)
-        print()
+        stack.append(big_task)
+        stack += buffer
 
-    final_schedule, benefit = find_schedule(leftovers) 
+    final_schedule, final_benefit, final_duration = find_schedule(stack) 
+
+    #print schedule
     for task in final_schedule:
         print(task)
-    print(benefit)
-    print(sum([task.get_duration() for task in final_schedule]))
+    print()
+    print("benefit:", final_benefit)
+    print("duration:", final_duration)
 
-    #print output
-    schedule_ids = [task.get_task_id() for task in big_schedule]
-    return schedule_ids
+    #expand big tasks to their individual tasks
+    expanded_final_schedule = []
+    for task in final_schedule:
+        if task in big_task_dict:
+            expanded_final_schedule += big_task_dict[task]
+        else:
+            expanded_final_schedule.append(task)
+
+    return [task.get_task_id() for task in expanded_final_schedule]
 
 def main():
     for input_path in os.listdir('inputs/'):
